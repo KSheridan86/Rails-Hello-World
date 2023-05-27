d3e0a7daa8c7f425edc99e4cecd84f4c0b6975e1
Ken Sheridan <kensheridan86@gmail.com>
2023-05-27 10:13:35 +0100

    attempting routes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,7 @@ class WelcomeController < ApplicationController
   def index
     @welcome = Welcome.all
   end
+  def show
+    @welcome = Welcome.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
+# Rails.application.routes.draw do
+#   root "welcome#index"
+#   get 'welcome/index'
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+# end
+
 Rails.application.routes.draw do
   root "welcome#index"
-  get 'welcome/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/welcomes", to: "welcomes#index"
+  get "/welcomes/:id", to: "welcomes#show"
 end
